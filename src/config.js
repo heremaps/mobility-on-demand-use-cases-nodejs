2fac6967ee70c054bb39ba823e7264ddc62cb219
Andreas May <andreas.may@here.com>
2017-03-17 16:05:10 +0100

    Fail early in case dummy credentials have not been replaced

--- src/config.js
+++ src/config.js
@@ -3,9 +3,14 @@
  * All rights reserved.
  */
 
+const assert = require('assert');
+
 const config = {
   app_id: 'YOUR_APP_ID', // Replace with your app_id
   app_code: 'YOUR_APP_CODE', // Replace with your app_code
 };
+
+assert.notEqual(config.app_id, 'YOUR_APP_ID', 'Please add your own app_id and app_code');
+assert.notEqual(config.app_code, 'YOUR_APP_CODE', 'Please add your own app_id and app_code');
 
 module.exports = config;
